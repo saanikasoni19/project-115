e4e078e13b7221302eb78d91cd41b70377869205
saanikasoni19 <88622734+saanikasoni19@users.noreply.github.com>
2023-04-29 11:18:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function setup(){
     video.hide();
     
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', getPoses);
+    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
